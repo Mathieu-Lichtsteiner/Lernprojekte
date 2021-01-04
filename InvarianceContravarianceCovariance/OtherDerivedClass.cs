f0159a0e98091d03106660f51082f9bf9a05f9e9
Mathieu Lichtsteiner <m.lichtsteiner@outlook.de>
2021-01-04 22:12:57 +0100

    I dont have to hide the properties if a derived class implements the interface used within the baseclass

---
+++ InvarianceContravarianceCovariance/OtherDerivedClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace InvarianceContravarianceCovariance {
+	internal class OtherDerivedClass : BaseClass<OtherClass>, IMethodWithType<OtherClass> {
+		public override void TestMethod4( OtherClass item ) => throw new NotImplementedException();
+		protected override void TestMethod1<TM>( TM item ) => throw new NotImplementedException();
+		protected override void TestMethod2( object item ) => throw new NotImplementedException();
+		protected override void TestMethod3( OtherClass item ) => throw new NotImplementedException();
+	}
+}
